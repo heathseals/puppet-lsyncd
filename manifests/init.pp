3fc2092f2fc81cb6011ec563f2ca75f2693b0597
Heath Seals <heath@puppetlabs.com>
2014-03-14 19:34:44 -0500

    clean up variable names.  still a mess.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,21 +6,20 @@
 #  class { 'lsyncd':  }
 #
 class lsyncd (
-  $sync_source       = $::lsyncd::params::sync_source,
-  $sync_target       = $::lsyncd::params::sync_target,
-  $active            = $::lsyncd::params::active,
-  $excludes          = $::lsyncd::params::excludes,
-  $lsyncd            = $::lsyncd::params::lsyncd,
-  $lsyncd_config     = $::lsyncd::params::lsyncd_config,
-  $lsyncd_config_tpl = $::lsyncd::params::lsyncd_config_tpl,
-  $lsyncd_dir        = $::lsyncd::params::lsyncd_dir,
-  $lsyncd_excludes   = $::lsyncd::params::lsyncd_excludes,
-  $excludes_list     = $::lsyncd::params::excludes_list,
-  $excludes_list_tpl = $::lsyncd::params::excludes_list_tpl,
-  $logfile           = $::lsyncd::params::logfile,
-  $statusfile        = $::lsyncd::params::statusfile,
-  $rsh               = $::lsyncd::params::rsh,
-  $rsyncOpts         = $::lsyncd::params::rsyncOpts,
+  $sync_source   = $::lsyncd::params::sync_source,
+  $sync_target   = $::lsyncd::params::sync_target,
+  $active        = $::lsyncd::params::active,
+  $lsyncd        = $::lsyncd::params::lsyncd,
+  $config        = $::lsyncd::params::config,
+  $config_tpl    = $::lsyncd::params::config_tpl,
+  $config_dir    = $::lsyncd::params::config_dir,
+  $excludes      = $::lsyncd::params::excludes,
+  $excludes_tpl  = $::lsyncd::params::excludes_tpl,
+  $excludes_file = $::lsyncd::params::excludes_file,
+  $logfile       = $::lsyncd::params::logfile,
+  $statusfile    = $::lsyncd::params::statusfile,
+  $rsh           = $::lsyncd::params::rsh,
+  $rsyncOpts     = $::lsyncd::params::rsyncOpts,
 ) inherits lsyncd::params {
 
   File {
@@ -38,15 +37,15 @@ class lsyncd (
     require   => Package[$lsyncd],
   }
 
-  file { $lsyncd_config:
-    content => template($lsyncd_config_tpl),
+  file { $config:
+    content => template($config_tpl),
     notify  => Service[$lsyncd],
     require => Package[$lsyncd],
   }
 
-  if ($excludes == true) {
-    file { $lsyncd_excludes:
-    content => template($excludes_list_tpl),
+  unless $excludes == '' {
+    file { $excludes_file:
+    content => template($excludes_tpl),
     notify  => Service[$lsyncd],
     require => Package[$lsyncd],
     }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,27 +1,27 @@
 class lsyncd::params {
 
-  $sync_source        = ''
-  $sync_target        = ''
-  $active             = true
-  $excludes           = false
-  $lsyncd             = 'lsyncd'
-  $lsyncd_dir         = '/etc/lsyncd'
-  $lsyncd_config_tpl  = 'lsyncd/lsyncd.conf.erb'
-  $excludes_list      = []
-  $excludes_list_tpl  = 'lsyncd/lsyncd-excludes.erb'
-  $logfile            = '/var/log/lsyncd.log'
-  $statusfile         = '/var/log/lsyncd.status'
-  $rsh                = ''
-  $rsyncOpts          = ''
+  $sync_source  = ''
+  $sync_target  = ''
+  $active       = true
+  $lsyncd       = 'lsyncd'
+  $config_tpl   = 'lsyncd/lsyncd.conf.erb'
+  $excludes     = []
+  $excludes_tpl = 'lsyncd/lsyncd-excludes.erb'
+  $logfile      = '/var/log/lsyncd.log'
+  $statusfile   = '/var/log/lsyncd.status'
+  $rsh          = ''
+  $rsyncOpts    = ''
 
   case $::osfamily {
     debian: {
-      $lsyncd_config      = '/etc/lsyncd/lsyncd.conf.lua'
-      $lsyncd_excludes    = '/etc/lsyncd/lsyncd-excludes'
+      $config_dir    = '/etc/lsyncd'
+      $config        = '/etc/lsyncd/lsyncd.conf.lua'
+      $excludes_file = '/etc/lsyncd/lsyncd-excludes'
     }
     redhat: {
-      $lsyncd_config      = '/etc/lsyncd.conf'
-      $lsyncd_excludes    = '/etc/lsyncd-excludes'
+      $config_dir    = '/etc'
+      $config        = '/etc/lsyncd.conf'
+      $excludes_file = '/etc/lsyncd-excludes'
     }
     default: {
       fail("not supported on an ${::osfamily}.")
